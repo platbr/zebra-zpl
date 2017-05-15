50003893a03b13d97b1cdb3ba4e4265c32334b43
Wagner Caixeta <wagner.caixeta@gmail.com>
2017-05-15 00:59:52 -0300

    Added image support.

---
+++ lib/zebra/zpl/image.rb
@@ -0,0 +1,40 @@
+require "zebra/zpl/printable"
+require 'labelary'
+
+module Zebra
+  module Zpl
+    class Image
+      include Printable
+
+      attr_writer :width
+      attr_writer :size, :file_path, :base64_image
+
+      def to_zpl
+        @data = Base64.decode64(@base64_image) if @base64_image
+        @data = File.binread(@file_path) if @file_path
+        check_attributes
+        cache_img_file = File.join(Rails.root, 'tmp', 'zebra_zpl_img' + Digest::SHA1.hexdigest(@data) + "_" + @size)
+        if File.exist?(cache_img_file) && !File.zero?(cache_img_file)
+          ready_img = File.binread(cache_img_file)
+        else
+          image = MiniMagick::Image.read(@data)
+          image.flatten
+          image.colorspace 'gray'
+          image.monochrome
+          image.resize(@size)
+          image.extent "#{(image.width/8.0).round*8}x#{(image.height/8.0).round*8}"
+          ready_img = Labelary::Image.encode path: image.path, filename: 'logo.png', mime_type: 'image/png'
+        end
+        %(^FO#{x},#{y},#{ready_img})
+      end
+
+      private
+
+      def check_attributes
+        super
+        # raise MissingAttributeError.new("file path to be used is not given") unless @file_path
+        raise MissingAttributeError.new("size to be used is not given") unless @size
+      end
+    end
+  end
+end

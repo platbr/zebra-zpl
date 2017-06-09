c3173965d537b80110aea9f4d3a1478d52635120
Wagner Caixeta <wagner.caixeta@gmail.com>
2017-06-09 00:17:06 -0300

    Ajuste

--- lib/zebra/zpl/image.rb
+++ lib/zebra/zpl/image.rb
@@ -13,9 +13,10 @@ module Zebra
         @data = Base64.decode64(@base64_image) if @base64_image
         @data = File.binread(@file_path) if @file_path
         check_attributes
-        cache_img_file = File.join(Rails.root, 'tmp', "zebra_zpl_img_#{Digest::SHA1.hexdigest(@data)}_#{@size}")
-        if File.exist?(cache_img_file) && !File.zero?(cache_img_file)
-          image = MiniMagick::Image.read(File.binread(cache_img_file))
+        cache_img_path = File.join(Rails.root, 'tmp', "zebra_zpl_img_#{Digest::SHA1.hexdigest(@data)}_#{@size}")
+
+        if File.exist?(cache_img_path) && !File.zero?(cache_img_path)
+          @file = MiniMagick::Image.new(cache_img_path)
         else
           image = MiniMagick::Image.read(@data)
           image.flatten
@@ -23,16 +24,16 @@ module Zebra
           image.monochrome
           image.resize(@size)
           image.extent "#{(image.width/8.0).ceil*8}x#{(image.height/8.0).ceil*8}"
-          file = File.open(cache_img_file, 'w+')
-          file.binmode
-          file.write File.binread(image.path)
-          file.close
+          @file = File.open(cache_img_path, 'w+')
+          @file.binmode
+          @file.write File.binread(image.path)
+          @file.close
         end
-        
-        image_zpl = Labelary::Image.encode path: image.path, filename: 'image.png', mime_type: 'image/png'
+
+        image_zpl = Labelary::Image.encode path: @file.path, filename: 'image.png', mime_type: 'image/png'
 
         if justification == Justification::CENTER
-          x = (@width - image.width)/2
+          x = (@width - @file.width)/2
         end
 
         %(^FO#{x},#{y},#{image_zpl})
